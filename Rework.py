9f064ad658bd7fa14f0e22bc7e6fff6f1a9ec9b0
Marvin <alican.karatasli@hotmail.com>
2023-10-20 11:21:24 +0200

    Neuer Algorithmus zur Feldlänge

--- Rework.py
+++ Rework.py
@@ -1,3 +1,21 @@
 import random
 import secrets
+import math
+
+input_text = input("Geben Sie hier den Text ein um diesen zu verschlüsseln: ")
+
+binary_clear_text = []
+for c in input_text:
+    binary_clear_text.append(bin(ord(c))[2:])
+
+# Testfall
+print(binary_clear_text)
+
+field_length = len(binary_clear_text)
+if field_length < 10:
+    field_length = 10
+
+field_length = 9 ** math.ceil(math.log(field_length, 9))
+
+print(field_length)
 
